d363c7f53a4bf5fd2d41cb86320342f9cf918be4
SSithole <50705022+sssithole@users.noreply.github.com>
2021-06-02 14:00:01 +0200

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,50 @@
+function addRow() {
+          
+    var mystork = document.getElementById("stork");
+    var price = document.getElementById("price");
+    var quntity = document.getElementById("Quntity");
+    var table = document.getElementById("myTableData");
+ 
+    var rowCount = table.rows.length;
+    var row = table.insertRow(rowCount);
+ 
+    row.insertCell(0).innerHTML= '<input type="button" value = "Delete" onClick="Javacsript:deleteRow(this)">';
+    row.insertCell(1).innerHTML= mystork.value;
+    row.insertCell(2).innerHTML= price.value;
+    row.insertCell(3).innerHTML= quntity.value;
+ 
+}
+ 
+function deleteRow(obj) {
+      
+    var index = obj.parentNode.parentNode.rowIndex;
+    var table = document.getElementById("myTableData");
+    table.deleteRow(index);
+    
+}
+ 
+function addTable() {
+      
+    var myTableDiv = document.getElementById("myDynamicTable");
+      
+    var table = document.createElement('TABLE');
+    table.border='1';
+    
+    var tableBody = document.createElement('TBODY');
+    table.appendChild(tableBody);
+      
+    for (var i=0; i<3; i++){
+       var tr = document.createElement('TR');
+       tableBody.appendChild(tr);
+       
+       for (var j=0; j<4; j++){
+           var td = document.createElement('TD');
+           td.width='75';
+           td.appendChild(document.createTextNode("Cell " + i + "," + j));
+           tr.appendChild(td);
+       }
+    }
+    myTableDiv.appendChild(table);
+    
+}
+ 
